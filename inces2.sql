99927ea39d9b99c2bcc6bfdfc4b32ee7d177ce74
JeisonBalduz <jeisonbalduz12@gmail.com>
2024-03-26 17:41:55 -0400

    incorporacion de BD

--- inces2.sql
+++ inces2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-03-2024 a las 06:24:50
+-- Tiempo de generación: 26-03-2024 a las 22:28:22
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
